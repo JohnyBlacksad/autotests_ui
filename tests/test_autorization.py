2b6ecf6551a25410ab2be6e5ed7f255814b0b079
JohnyBlacksad <vergulofthedevil@gmail.com>
2025-09-19 00:46:25 +0500

    parametrize mark

--- tests/test_autorization.py
+++ tests/test_autorization.py
@@ -1,6 +1,9 @@
 from playwright.sync_api import expect, Page
 import pytest
 
+#Переписал функцию согласно задаче, изменил способ получения локаторов и заголовок функции.
+
+""" 
 @pytest.mark.regression
 @pytest.mark.authorization
 def test_authorization(chromium_page: Page):
@@ -16,5 +19,29 @@ def test_authorization(chromium_page: Page):
         button_locator.click()
         
         expect(error_locator).to_be_visible()
-        expect(error_locator).to_have_text("Wrong email or password")
+        expect(error_locator).to_have_text("Wrong email or password") """
         
+
+
+users = {('user.name@gmail.com', 'password'): 'Invalid creeds',
+        ('user.name@gmail.com', '  '): 'Two spaces in password', 
+        ('  ', 'password'): 'Two spaces in email'}
+
+@pytest.mark.authorization
+@pytest.mark.parametrize('email, password', 
+                         users.keys(),
+                         ids=users.values())
+def test_wrong_email_or_password_authorization(chromium_page: Page, email: str, password: str):
+        chromium_page.goto('https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/auth/login')
+        email_input = chromium_page.get_by_test_id('login-form-email-input').locator('input')
+        email_input.fill('user.name@gmail.com')
+        
+        password_input = chromium_page.get_by_test_id('login-form-password-input').locator('input')
+        password_input.fill('password')
+        
+        login_button = chromium_page.get_by_test_id('login-page-login-button')
+        login_button.click()
+        
+        wrong_allert = chromium_page.get_by_test_id('login-page-wrong-email-or-password-alert')
+        expect(wrong_allert).to_be_visible()
+        expect(wrong_allert).to_have_text('Wrong email or password')
