537622771be27f4d568b329d209fc285a5938b47
JohnyBlacksad <vergulofthedevil@gmail.com>
2025-09-24 01:18:37 +0500

    Getting to Know PageFactory

---
+++ elements/input.py
@@ -0,0 +1,14 @@
+from elements.base_element import BaseElement
+from playwright.sync_api import expect
+
+class Input(BaseElement):
+    def get_locator(self, **kwargs):
+        return super().get_locator(**kwargs).locator('input')
+    
+    def fill(self, value: str, **kwargs) -> None:
+        locator = self.get_locator(**kwargs)
+        locator.fill(value)
+        
+    def check_have_value(self, value: str, **kwargs) -> None:
+        locator = self.get_locator(**kwargs)
+        expect(locator).to_have_value()
--- pages/login_page.py
+++ pages/login_page.py
@@ -1,15 +1,18 @@
 from components.authentication.login_form_component import LoginFormComponent
+from elements.button import Button
+from elements.link import Link
+from elements.text import Text
 from pages.base_page import BasePage
-from playwright.sync_api import Page, expect
+from playwright.sync_api import Page
 
 class LoginPage(BasePage):
     def __init__(self, page: Page) -> None:
         super().__init__(page)
         self.login_form = LoginFormComponent(page)
        
-        self.login_button = page.get_by_test_id('login-page-login-button')
-        self.registration_link = page.get_by_test_id('login-page-registration-link')
-        self.error_allert = page.get_by_test_id('login-page-wrong-email-or-password-alert')
+        self.login_button = Button(page, 'login-page-login-button', 'Login')
+        self.registration_link = Link(page, 'login-page-registration-link', 'Registration')
+        self.error_allert = Text(page, 'login-page-wrong-email-or-password-alert', 'Wrong email or password')
         
     def click_login_button(self) -> None:
         self.login_button.click()
@@ -18,6 +21,6 @@ class LoginPage(BasePage):
         self.registration_link.click()
     
     def check_allert_visible(self):
-        expect(self.error_allert).to_be_visible()
-        expect(self.error_allert).to_have_text('Wrong email or password')
+        self.error_allert.check_visible()
+        self.error_allert.check_have_text('Wrong email or password')
     
