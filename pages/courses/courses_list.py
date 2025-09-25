4c9444e9c63fb95b122ef03746824bc60f457e27
JohnyBlacksad <vergulofthedevil@gmail.com>
2025-09-25 13:47:24 +0500

    Create test edit course

--- pages/courses/courses_list.py
+++ pages/courses/courses_list.py
@@ -5,7 +5,7 @@ from components.views.empty_view_component import EmptyViewComponent
 from components.courses.course_view_component import CourseViewComponents
 from pages.base_page import BasePage
 from playwright.sync_api import Page
-
+import re
 
 class CoursesListPage(BasePage):
     def __init__(self, page: Page):
@@ -23,3 +23,11 @@ class CoursesListPage(BasePage):
         
         self.empty_view.check_visible(title=title, description=description)
         
+    def check_courses_list(self, index: int, title: str, max_score: int, min_score: int, estimated_time: str):
+        self.side_bar.check_visible()
+        self.course_view.check_visible(index=index, title=title, max_score=max_score, min_score=min_score, estimated_time=estimated_time)
+        self.toolbar_view.check_visible()
+        
+    def click_edit_course(self, index: int):
+        self.course_view.cart_menu.click_edit(index=index)
+        self.check_current_url(re.compile(r'.*/#/courses/[^/#]+$'))
--- tests/courses/test_courses.py
+++ tests/courses/test_courses.py
@@ -66,3 +66,46 @@ class TestCourses:
                                                     estimated_time=course_time)
         
         courses_list_page.course_view.cart_menu.cilck_delete(index=0)
+    
+    def test_edit_course(self, created_courses_page: CreateCoursePage,  courses_list_page: CoursesListPage):
+       
+        created_courses_page.visit('https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/courses/create')
+        created_courses_page.image_upload_widget.check_visible(is_image_uploaded=False)
+        created_courses_page.course_form.check_visible()
+        created_courses_page.check_empty_task_list()
+        created_courses_page.image_upload_widget.upload_course_img(file='./testdata/files/image.jpg')
+        created_courses_page.image_upload_widget.check_visible(is_image_uploaded=True)
+        
+        created_courses_page.course_form.fill(
+            title='Title', estimated_time='4 weeks', description='Course description',
+            max_score='100', min_score='10'
+        )
+        
+        created_courses_page.exercises_toolbar.click_add_exercise_button()
+        
+        created_courses_page.course_exercise_form.fill(
+            title='Task 1', description='Task 1 descr', index=0)
+        
+        created_courses_page.course_exercise_form.check_visible(index=0, title='Task 1', description='Task 1 descr')
+        
+        created_courses_page.toolbar.check_visible(is_form_fill=True)
+        
+        created_courses_page.toolbar.click_create_button()
+        
+        courses_list_page.course_view.check_visible(
+            index=0, title='Title',estimated_time='4 weeks',
+            max_score='100', min_score='10')
+        
+        courses_list_page.click_edit_course(index=0)
+        
+        created_courses_page.course_form.fill(
+            title='Edit title', estimated_time='2 weeks', 
+            description='Description', max_score='10', min_score='1')
+        
+        created_courses_page.toolbar.check_visible(is_form_fill=True)
+        
+        created_courses_page.toolbar.click_create_button()
+        
+        courses_list_page.course_view.check_visible(index=0,
+            title='Edit title', estimated_time='2 weeks', 
+            max_score='10', min_score='1')
